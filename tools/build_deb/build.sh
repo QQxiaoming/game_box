d178e3be2071044615f4b83cb3443a7890062c0e
xiaoming <2014500726@smail.xtu.edu.cn>
2022-07-28 14:04:22 +0800

    build:update build_deb.sh
    Signed-off-by: xiaoming <2014500726@smail.xtu.edu.cn>

--- tools/build_deb/build.sh
+++ tools/build_deb/build.sh
@@ -1,31 +1,14 @@
 #!/bin/sh
 
-./linuxdeployqt ../../release/out/game_box_temp -appimage
-mkdir ../../release/out/plugins
-cp $Qt6_DIR/lib/libQt6XcbQpa.so.6 ../../release/out/lib/libQt6XcbQpa.so.6
-cp $Qt6_DIR/lib/libQt6OpenGL.so.6 ../../release/out/lib/libQt6OpenGL.so.6
-mkdir ../../release/out/plugins/bearer
-cp $Qt6_DIR/plugins/bearer/libqconnmanbearer.so ../../release/out/plugins/bearer/libqconnmanbearer.so
-cp $Qt6_DIR/plugins/bearer/libqgenericbearer.so ../../release/out/plugins/bearer/libqgenericbearer.so
-cp $Qt6_DIR/plugins/bearer/libqnmbearer.so ../../release/out/plugins/bearer/libqnmbearer.so
-mkdir ../../release/out/plugins/iconengines
-cp $Qt6_DIR/plugins/iconengines/libqsvgicon.so ../../release/out/plugins/iconengines/libqsvgicon.so
-mkdir ../../release/out/plugins/imageformats
-cp $Qt6_DIR/plugins/imageformats/libqgif.so ../../release/out/plugins/imageformats/libqgif.so
-cp $Qt6_DIR/plugins/imageformats/libqico.so ../../release/out/plugins/imageformats/libqico.so
-cp $Qt6_DIR/plugins/imageformats/libqjpeg.so ../../release/out/plugins/imageformats/libqjpeg.so
-cp $Qt6_DIR/plugins/imageformats/libqsvg.so ../../release/out/plugins/imageformats/libqsvg.so
-mkdir ../../release/out/plugins/platforminputcontexts
-cp $Qt6_DIR/plugins/platforminputcontexts/libcomposeplatforminputcontextplugin.so ../../release/out/plugins/platforminputcontexts/libcomposeplatforminputcontextplugin.so
-cp $Qt6_DIR/plugins/platforminputcontexts/libibusplatforminputcontextplugin.so ../../release/out/plugins/platforminputcontexts/libibusplatforminputcontextplugin.so
-mkdir ../../release/out/plugins/platforms
-cp $Qt6_DIR/plugins/platforms/libqxcb.so ../../release/out/plugins/platforms/libqxcb.so
-mkdir ../../release/out/plugins/xcbglintegrations
-cp $Qt6_DIR/plugins/xcbglintegrations/libqxcb-egl-integration.so ../../release/out/plugins/xcbglintegrations/libqxcb-egl-integration.so
-cp $Qt6_DIR/plugins/xcbglintegrations/libqxcb-glx-integration.so ../../release/out/plugins/xcbglintegrations/libqxcb-glx-integration.so
-rm -rf ../../release/out/default.desktop
-rm -rf ../../release/out/default.png
-rm -rf ../../release/out/game_box_temp
-rm -rf ../../release/out/doc
+export QMAKE=$Qt6_DIR/bin/qmake
+./linuxdeploy-x86_64.AppImage --executable=../../release/out/game_box_temp --appdir=../../release --plugin=qt
+rm -rf ../../release/apprun-hooks
+mv ../../release/out/game_box ../../release/usr/game_box
+rm -rf ../../release/out
+mv ../../release/usr ../../release/out
+mv ../../release/out/bin/qt.conf ../../release/out/qt.conf
+rm -rf ../../release/out/bin
+sed -i "s/Prefix = ..\//Prefix = .\//g" ../../release/out/qt.conf
+chrpath -r "\$ORIGIN/./lib" ../../release/out/game_box
+rm -rf ../../release/out/share
 cp ./game_box.sh ../../release/out/game_box.sh
-
